f94c9d627ce2c32bcb1ce70d16182605ae8eb165
Mark Jimenez <markjimenez@Marks-MacBook-Pro.local>
2019-10-27 16:49:31 -0400

    Update translate_team

--- draft_kings/utilities.py
+++ draft_kings/utilities.py
@@ -26,36 +26,130 @@ def dig(data, *key_path, transformer=identity, fallback=None):
 
 def translate_team(num):
     teams = [
-        "ATL",
-        "BOS",
-        "NOP",
-        "CHI",
-        "CLE",
-        "6",
-        "7",
-        "DET",
-        "9",
-        "HOU",
-        "IND",
-        "LAC",
-        "13",
-        "14",
-        "15",
-        "16",
-        "17",
-        "NYK",
-        "ORL",
-        "PHI",
-        "PHO",
-        "22",
-        "SAC",
-        "SAS",
-        "25",
-        "UTA",
-        "WAS",
-        "TOR",
-        "29",
-        "30"
+        {
+            "id": 1,
+            "team": "ATL"
+        },
+        {
+            "id": 2,
+            "team": "BOS"
+        },
+        {
+            "id": 3,
+            "team": "NOP"
+        },
+        {
+            "id": 4,
+            "team": "CHI"
+        },
+        {
+            "id": 5,
+            "team": "CLE"
+        },
+        {
+            "id": 6,
+            "team": "DAL"
+        },
+        {
+            "id": 7,
+            "team": "7"
+        },
+        {
+            "id": 8,
+            "team": "DET"
+        },
+        {
+            "id": 9,
+            "team": "9"
+        },
+        {
+            "id": 10,
+            "team": "HOU"
+        },
+        {
+            "id": 11,
+            "team": "IND"
+        },
+        {
+            "id": 12,
+            "team": "LAC"
+        },
+        {
+            "id": 13,
+            "team": "LAL"
+        },
+        {
+            "id": 14,
+            "team": "MIA"
+        },
+        {
+            "id": 15,
+            "team": "15"
+        },
+        {
+            "id": 16,
+            "team": "MIN"
+        },
+        {
+            "id": 17,
+            "team": "BKN"
+        },
+        {
+            "id": 18,
+            "team": "NYK"
+        },
+        {
+            "id": 19,
+            "team": "ORL"
+        },
+        {
+            "id": 20,
+            "team": "PHI"
+        },
+        {
+            "id": 21,
+            "team": "PHO"
+        },
+        {
+            "id": 22,
+            "team": "POR"
+        },
+        {
+            "id": 23,
+            "team": "SAC"
+        },
+        {
+            "id": 24,
+            "team": "SAS"
+        },
+        {
+            "id": 25,
+            "team": "25"
+        },
+        {
+            "id": 26,
+            "team": "UTA"
+        },
+        {
+            "id": 27,
+            "team": "WAS"
+        },
+        {
+            "id": 28,
+            "team": "TOR"
+        },
+        {
+            "id": 29,
+            "team": "MEM"
+        },
+        {
+            "id": 30,
+            "team": "30"
+        },
+        {
+            "id": 5312,
+            "team": "CHA"
+        }
     ]
 
-    return teams[num - 1]
+    return next(filter(lambda x: x.get("id") == num, teams))["team"]
